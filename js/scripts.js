43fe921b42c28a564bad413cde01ccaad2e088ba
TJun-Jie <tjunjie2000@gmail.com>
2020-07-12 00:39:06 +0800

    Generate 12 users on page reload

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,79 @@
 
 
-// Generating gallery 
-const gallery = document.querySelector('#gallery')
-const card = document.createElement('div');
-card.classList.add('card');
+// Generating 1 user's html
+function creatingCard(user) {
+    // variables
+    const gallery = document.querySelector('#gallery')
 
-const cardImageContainer = document.createElement('div');
-cardImageContainer.classList.add('card-img-container');
+    // card div
+    const card = document.createElement('div');
 
-const img = document.createElement('img');
-img.classList.add('card-img');
-cardImageContainer.appendChild(img)
+    // Image div
+    const cardImageContainer = document.createElement('div');
+    const img = document.createElement('img');
+
+    // card info div
+    const cardInfoContainer = document.createElement('div');
+    const cardName = document.createElement('h3');
+    const cardEmail = document.createElement('p');
+    const cardLocation = document.createElement('p');
+
+    // add classes to all the different sections and append them to the html
+    function generatingHtml() {
+        card.classList.add('card');
+        
+        cardImageContainer.classList.add('card-img-container');
+        
+        img.classList.add('card-img');
+        cardImageContainer.appendChild(img)
+        
+        cardName.classList.add('card-name');
+        cardName.classList.add('cap');
+        cardName.id = 'name';
+        
+        cardEmail.classList.add('card-text');
+        
+        cardLocation.classList.add('card-text');
+        cardLocation.classList.add('cap');
+        cardInfoContainer.appendChild(cardName);
+        cardInfoContainer.appendChild(cardEmail);
+        cardInfoContainer.appendChild(cardLocation);
+        
+        card.appendChild(cardImageContainer);
+        card.appendChild(cardInfoContainer)
+        
+        gallery.appendChild(card)
+
+    }
+
+    // Add the data received to the html
+    function createcard(user){
+        img.src = user.picture.large
+        cardName.textContent = `${user.name.first} ${user.name.last}`
+        cardEmail.textContent = `${user.email}`
+        cardLocation.textContent = user.location.state  
+    }
+    generatingHtml()
+    createcard(user)
+
+}
 
 
-const cardInfoContainer = document.createElement('div');
 
-const cardName = document.createElement('h3');
-cardName.classList.add('card-name');
-cardName.classList.add('cap');
-cardName.id = 'name';
-
-const cardEmail = document.createElement('p');
-cardEmail.classList.add('card-text');
-
-const cardLocation = document.createElement('p');
-cardLocation.classList.add('card-text');
-cardLocation.classList.add('cap');
-
-cardInfoContainer.appendChild(cardName);
-cardInfoContainer.appendChild(cardEmail);
-cardInfoContainer.appendChild(cardLocation);
-
-card.appendChild(cardImageContainer);
-card.appendChild(cardInfoContainer)
-
-gallery.appendChild(card)
-
-
-
-// <div class="card">
-// <div class="card-img-container">
-//     <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-// </div>
-// <div class="card-info-container">
-//     <h3 id="name" class="card-name cap">first last</h3>
-//     <p class="card-text">email</p>
-//     <p class="card-text cap">city, state</p>
-// </div>
-// </div>
-
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?results=12')
 .then( res => res.json())
-.then( data => addingUser(data))
+.then( data => gettingAllUsers(data))
 .catch(error => console.log('Looks like there was a problem!', error))
 
 
-function addingUser(data) {
-    img.src = data.results[0].picture.large
+
+function gettingAllUsers(data) {
+    const resultsArr = data.results;
+
+    for (let i=0; i< resultsArr.length; i++) {
+        creatingCard(resultsArr[i]);
+    
+
+    }
 }
+
