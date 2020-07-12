a111baabaf3641a5aa812d4a4f26f68a8b5d6fd9
TJun-Jie <tjunjie2000@gmail.com>
2020-07-12 23:51:38 +0800

    refactored code

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,7 @@
+//==============================
+//public API request 
+//==============================
 
-
-// Generating 1 user's html
-function creatingCard(user) {
-    // variables
-    const gallery = document.querySelector('#gallery')
-
-    // card div
-    const card = createElement('div');
-
-    // Image div
-    const cardImageContainer = createElement('div');
-    const imgCard = createElement('img');
-
-    // card info div
-    const cardInfoContainer = createElement('div');
-    const cardName = createElement('h3');
-    const cardEmail = createElement('p');
-    const cardLocation = createElement('p');
-
-    // add classes to all the different sections and append them to the html
-    function generatingCardHtml() {
-        card.classList.add('card');
-        
-        cardImageContainer.classList.add('card-img-container');
-        
-        imgCard.classList.add('card-img');
-        cardImageContainer.appendChild(imgCard);
-        
-        cardName.classList.add('card-name');
-        cardName.classList.add('cap');
-        cardName.id = 'name';
-        
-        cardEmail.classList.add('card-text');
-        
-        cardLocation.classList.add('card-text');
-        cardLocation.classList.add('cap');
-        cardInfoContainer.appendChild(cardName);
-        cardInfoContainer.appendChild(cardEmail);
-        cardInfoContainer.appendChild(cardLocation);
-        
-        card.appendChild(cardImageContainer);
-        card.appendChild(cardInfoContainer)
-        
-        gallery.appendChild(card)
-
-    }
-
-    // Add the data received to the html
-    function createcard(user){
-        imgCard.src = user.picture.large
-        cardName.textContent = `${user.name.first} ${user.name.last}`
-        cardEmail.textContent = `${user.email}`
-        cardLocation.textContent = `${user.location.state}, ${user.location.country}`  
-    }
-    generatingCardHtml()
-    createcard(user)
-
-
-
-
-    
-
-
-
-}
-
-function createElement(element) {
-    return document.createElement(element);
-}
-function addClass(element, selectedClass) {
-    element.classList.add(selectedClass);
-}
 
 fetch('https://randomuser.me/api/?results=12')
 .then( res => res.json())
@@ -80,155 +11,153 @@ fetch('https://randomuser.me/api/?results=12')
 .catch(error => console.log('Looks like there was a problem!', error))
 
 
-
+// main function which receives the data of all 12 users
 function gettingAllUsers(data) {
-    const resultsArr = data.results;
-
-    for (let i=0; i< resultsArr.length; i++) {
-        creatingCard(resultsArr[i]);
-        addModalWindow(resultsArr[i])
-        addCardEventListener(i)
-        
-
+    const users = data.results;
+    let cardMarkup = ``;
+    // iterating through every single users
+    for (let i=0; i< users.length; i++) {
+        // Loop through all users and add all their html together
+        cardMarkup +=creatingCard(users[i]);
+        // Create modal div for each user
+        modalEvents(users[i]);
+          
     }
+    // Add html of all the user's card to the gallery
+    document.querySelector('#gallery').innerHTML = cardMarkup;
+    // add click listener to open modal window when clicked
+    addAllCardEventListener();
+
+    // create search bar
+    searchBar()
+
+    // add search bar functionality
+    searchEmployees()
+    
+}
+
+// Generating 1 user's html
+function creatingCard(user) {
+
+    return `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${user.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${user.location.state}, ${user.location.country}</p>
+        </div>
+    </div>
+    `    
+
 }
 
 
+function modalEvents(userData) {
+    // create modal div
+    const modal  = document.createElement('div')
+    modal.classList.add('modal-container')
+    // make the modal div invisible initially and only show it when user clicks the card
+    modal.style.display = 'none'
+    // add user's data into modal 
+    modal.innerHTML = addModalWindow(userData)
+    // Add modal div to the body html
+    document.querySelector('body').appendChild(modal)  
 
-function addModalWindow(user) {
-    // Creating modal elements
-    const modalContainer = createElement('div');
-    const modal = createElement('div');
-    const button =  createElement('button');
-    const modalInfoContainer = createElement('div');
-    const imgModal  = createElement('img');
-    const modalName = createElement('h3');
-    const modalEmail = createElement('p');
-    const modalCity = createElement('p');
-    const hr = createElement('hr');
-    const modalPhone =createElement('p');
-    const modalAddress = createElement('p');
-    const modalBirthday = createElement('p');
-    const modalButtonContainer = createElement('div');
-    const modalButtonPrev = createElement('button');
-    const modalButtonNext=  createElement('button');
-
-//     <div class="modal-btn-container">
-//     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-//     <button type="button" id="modal-next" class="modal-next btn">Next</button>
-// </div>
-
-    function createModalHtml() {
-        addClass(modalContainer, 'modal-container');
-        addClass(modal, 'modal');
-        addClass(button, 'modal-close-btn');
-        button.id = 'modal-close-btn';
-        addClass(modalInfoContainer, 'modal-info-container');
-        addClass(imgModal, 'modal-img');
-        addClass(modalName, 'modal-name');
-        addClass(modalName, 'cap');
-        modalName.id = 'name';
-        addClass(modalEmail, 'modal-text');
-        addClass(modalCity, 'modal-text');
-        addClass(modalPhone, 'modal-text')
-        addClass(modalAddress, 'modal-text')
-        addClass(modalBirthday, 'modal-text')
-        modalContainer.appendChild(modal);
-        modal.appendChild(button);
-        modal.appendChild(modalInfoContainer);
-        modalInfoContainer.appendChild(imgModal);
-        modalInfoContainer.appendChild(modalName);
-        modalInfoContainer.appendChild(modalEmail);
-        modalInfoContainer.appendChild(modalCity);
-        modalInfoContainer.appendChild(hr);
-        modalInfoContainer.appendChild(modalPhone);
-        modalInfoContainer.appendChild(modalAddress);
-        modalInfoContainer.appendChild(modalBirthday);
-        button.innerHTML= `<strong>X</strong>`
-
-        modalButtonContainer.classList.add('modal-btn-container');
-        modalButtonPrev.type = 'button';
-        modalButtonNext.type = 'button';
-        modalButtonPrev.id = 'modal-prev';
-        modalButtonNext.id = 'modal-next';
-        modalButtonPrev.classList.add('modal-prev', 'button');
-        modalButtonNext.classList.add('modal-next', 'button');
-        modalButtonPrev.textContent = 'Prev';
-        modalButtonNext.textContent = 'Next';
-        modalButtonContainer.appendChild(modalButtonPrev)
-        modalButtonContainer.appendChild(modalButtonNext)
-        modalContainer.appendChild(modalButtonContainer);
-        
-        
-
-    
-        document.querySelector('body').appendChild(modalContainer);
-        
-
-    }
-    function addModalInfo(user) {
-        imgModal.src = user.picture.large
-        modalName.textContent = `${user.name.first} ${user.name.last}`
-        modalEmail.textContent= user.email;
-        modalCity.textContent = user.location.city;
-        // formatting of phone numbers
-        const phone = user.phone;
-        const allNumbers = phone.replace(/[\D]/g , '')
-        const formatPhone = `(${allNumbers.slice(0,3)}) ${allNumbers.slice(3,6)}-${allNumbers.slice(6)}`
-        modalPhone.textContent = formatPhone
-        console.log(user.location)
-        modalAddress.textContent = `${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.nat} ${user.location.postcode}`
-        let bday = user.dob.date.split('T')[0];
-        let formatBday = bday.split('-');
-        const formattedBday = `${formatBday[2]}/${formatBday[1]}/${formatBday[0]}`
-        modalBirthday.textContent = `Birthday: ${formattedBday}`
-        modalContainer.style.display = 'none'
-    }
-
-    button.addEventListener('click', (e) => {
-        modalContainer.style.display = 'none';
+    // adding close button event listener
+    const closeButton  = modal.querySelector('#modal-close-btn')
+    closeButton.addEventListener('click', (e) => {
+        modal.style.display = 'none';
     })
 
+    // adding prev button event listener
+    const modalButtonPrev = modal.querySelector('#modal-prev');
     modalButtonPrev.addEventListener('click', (e) => {
-        prevModal = modalContainer.previousElementSibling
-        modalContainer.style.display = 'none'
+        const prevModal = modal.previousElementSibling
+        modal.style.display = 'none'
         if(prevModal) {
             prevModal.style.display = "block";
         }
     })
+    // adding next button event listener
+    const modalButtonNext = modal.querySelector('#modal-next');
     modalButtonNext.addEventListener('click', (e) => {
-        nextModal = modalContainer.nextElementSibling
-        modalContainer.style.display = 'none'
+        const nextModal = modal.nextElementSibling
+        modal.style.display = 'none'
         if(nextModal) {
             nextModal.style.display = "block";
         }
     })
-
-    addModalInfo(user)
-    createModalHtml()
-
 }
 
+// Receives user's data and create modal's html together with the user's data
+function addModalWindow(user) {
+    // format phone 
+    const phone = user.phone;
+    const allNumbers = phone.replace(/[\D]/g , '')
+    const formatPhone = `(${allNumbers.slice(0,3)}) ${allNumbers.slice(3,6)}-${allNumbers.slice(6)}`
 
-
-function addCardEventListener(index) {
+    // format bday
+    let bday = user.dob.date.split('T')[0];
+    let formatBday = bday.split('-');
+    
+    return `
+    <div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+    <p class="modal-text">${user.email}</p>
+    <p class="modal-text cap">${user.location.city}</p>
+    <hr>
+    <p class="modal-text">${formatPhone}</p>
+            <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.nat} ${user.location.postcode}</p>
+            <p class="modal-text">${formatBday[2]}/${formatBday[1]}/${formatBday[0]}</p>
+            </div>
+            </div>
+            <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+            `;
+                      
+        
+                    }
+                    
+                    
+// add click listener on 1 card to open modal when clicked
+function addOneCardEventListener(index) {
     const card = document.querySelectorAll('.card');
+    
+    // link the modal div to the card section
     const modalContainer = document.querySelectorAll('.modal-container')
     card[index].addEventListener('click', () => {
+        console.log('clicked')
         modalContainer[index].style.display = 'block';
     })
 }
 
+//  add click listener to all cards
+function addAllCardEventListener() {
+    const card = document.querySelectorAll('.card');
+    for (let i = 0 ; i < card.length ; i ++ ) {
+        addOneCardEventListener(i);
+    }
+}
+
+// create search bar 
 function searchBar() {
     const searchBarHtml = `<form action="#" method="get">
     <input type="search" id="search-input" class="search-input" placeholder="Search...">
     <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-</form>`;
+    </form>`;
     const searchDiv = document.querySelector('.search-container');
     searchDiv.innerHTML = searchBarHtml;
-
+    
 }
-searchBar()
+
 
 function searchEmployees() {
     const searchForm = document.querySelector('form');
@@ -244,6 +173,7 @@ function searchEmployees() {
         // There is something in the search
         else {          
             for ( let i = 0 ; i < allCards.length; i ++ ) {
+                // if input matches card name then we will show it , if it does not match, we will hide that card
                 const cardName = allCards[i].querySelector('.card-name').textContent;
                 // accepts cases where user types lower case or with upper casing
                 if(!cardName.toLowerCase().includes(searchInput.value) || !cardName.toLowerCase().includes(searchInput.value)) {
@@ -251,9 +181,8 @@ function searchEmployees() {
                 }
             }
         }
-    })
-    
+    })   
 }
-searchEmployees()
+
 
 
